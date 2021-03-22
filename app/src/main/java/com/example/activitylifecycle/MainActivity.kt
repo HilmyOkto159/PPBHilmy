af9531df4a165877c6beaaf34067fbf9aa4ca912
HilmyOkto <hilmy.oktoharitsa.tif18@polban.ac.id>
2021-03-22 15:00:23 +0700

    ppbo2

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.activitylifecycle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("lifecyscle", "onCreate invoked")
+        Toast.makeText(this, "onCreate invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d("lifecycle", "onStart invoked")
+        Toast.makeText(this, "onStart invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("lifecycle", "onResume invoked")
+        Toast.makeText(this, "onResume invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("lifecycle", "onPause invoked")
+        Toast.makeText(this, "onPause invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("lifecycle", "onStop invoked")
+        Toast.makeText(this, "onStop invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        Log.d("lifecycle", "onRestart invoked")
+        Toast.makeText(this,"onRestart invoked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        Log.d("lifecycle", "onDestroy invoked")
+        Toast.makeText(this, "onDestroy invoked", Toast.LENGTH_SHORT).show()
+    }
+
+}
